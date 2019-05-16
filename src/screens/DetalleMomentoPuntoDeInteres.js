41b5be4f5b6e91e3e55768063ffb9530c5cf16c7
Tomas Gonzalez <tomasgp14@hotmail.com>
2019-05-16 06:49:20 -0400

    overnight 6

--- src/screens/DetalleMomentoPuntoDeInteres.js
+++ src/screens/DetalleMomentoPuntoDeInteres.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import styled from "styled-components";
 import moment from "moment";
+import IosThumbsUp from "react-ionicons/lib/IosThumbsUp";
+import IosThumbsDown from "react-ionicons/lib/IosThumbsDown";
 
 import qs from "query-string";
 import gql from "graphql-tag";
@@ -9,19 +11,20 @@ import { compose } from "redux";
 
 import MdPerson from "react-ionicons/lib/MdPerson";
 
+import Arrow from "react-ionicons/lib/MdArrowRoundForward";
+
 const MainContainer = styled.div`
-  display: flex;
   height: 100vh;
   background-color: #c3dffa;
   padding-left: 10px;
   padding-right: 10px;
+  padding-top: 60px;
 `;
 
 const ItemContainer = styled.div`
   display: flex;
+  flex-direction: column;
   flex: 1;
-  margin-top: 60px;
-  height: 400px;
   background-color: white;
 `;
 
@@ -32,10 +35,13 @@ const Header = styled.div`
   padding-top: 10px;
   padding-left: 10px;
   padding-right: 10px;
-  background-color: red;
 `;
 
 class DetalleMomentoPuntoDeInteres extends PureComponent {
+  state = {
+    newComment: null
+  };
+
   render() {
     if (!this.props.data.momentos) {
       return <div>Loading...</div>;
@@ -84,8 +90,95 @@ class DetalleMomentoPuntoDeInteres extends PureComponent {
                 @{item.usuario.nombreDeUsuario && item.usuario.nombreDeUsuario}
               </p>
             </div>
-            <div>{moment(item.fechaDeCreacion).fromNow()}</div>
+            <div style={{ color: "#F7C636", fontWeight: "bold" }}>
+              {moment(item.fechaDeCreacion).fromNow()}
+            </div>
           </Header>
+          <img style={{ width: "100%" }} src={item.multimedia} />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "100%",
+              padding: 10
+            }}
+          >
+            <div>En la zona</div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                marginRight: 5
+              }}
+            >
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <IosThumbsUp color={"#4B98F4"} />
+                <p style={{ fontSize: 8, color: "#4B98F4", marginTop: 5 }}>
+                  {item.likes}
+                </p>
+              </div>
+
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <IosThumbsDown color={"#EB4826"} />
+                <p style={{ fontSize: 8, color: "#EB4826", marginTop: 5 }}>
+                  {item.dislikes}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div style={{ backgroundColor: "#4797F4" }}>{item.descripcion}</div>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <input placeholder="Type your comment" style={{ width: "100%" }} />
+            <div
+              onClick={() => {
+                console.log("good mithical morning");
+              }}
+              style={{ paddingLeft: "10px", paddingRight: "10px" }}
+            >
+              <Arrow fontSize="20px" color="#90BEF8" />
+            </div>
+          </div>
+          {item.comentarios.edges.map(() => {
+            return (
+              <div
+                style={{
+                  backgroundColor: "#4797F4",
+                  padding: 10,
+                  display: "flex",
+                  flexDirection: "row"
+                }}
+              >
+                {item.usuario.urlFotoMiniatura ? (
+                  <img
+                    alt=""
+                    style={{
+                      height: 30,
+                      width: 30,
+                      borderRadius: 30,
+                      backgroundColor: "#C3DFFA"
+                    }}
+                    src={item.usuario.urlFotoMiniatura}
+                  />
+                ) : (
+                  <div
+                    style={{
+                      height: 30,
+                      width: 30,
+                      borderRadius: 30,
+                      backgroundColor: "#C3DFFA",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center"
+                    }}
+                  >
+                    <MdPerson fontSize="20px" color="#90BEF8" />
+                  </div>
+                )}
+                <div>@ helel</div>
+              </div>
+            );
+          })}
         </ItemContainer>
       </MainContainer>
     );
@@ -99,6 +192,13 @@ const query = gql`
         email
         nombreDeUsuario
         urlFotoMiniatura
+      }
+      comentarios {
+        edges {
+          node {
+            idComentario
+          }
+        }
       }
       idMomento
       multimedia
